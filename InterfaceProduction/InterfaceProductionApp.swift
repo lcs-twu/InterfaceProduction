89f0db560ca45b2ebb78823f787d0a565303f4cf
Tom <twu@lcs.on.ca>
2022-11-17 09:16:01 -0500

    Initial Commit

---
+++ InterfaceProduction/InterfaceProductionApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterfaceProductionApp.swift
+//  InterfaceProduction
+//
+//  Created by Tom Wu on 2022-11-17.
+//
+
+import SwiftUI
+
+@main
+struct InterfaceProductionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
